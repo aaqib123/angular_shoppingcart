6c005fd9c9bbc1b16437e65688c08327d968ee33
Aaqib <aaqib.hyd@gmail.com>
2018-07-29 18:14:47 -0300

    setup again

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAPsG8Fw9kN0uvqBHFB5tDeeGvTVzTilDE",
+    authDomain: "oshop-d352e.firebaseapp.com",
+    databaseURL: "https://oshop-d352e.firebaseio.com",
+    projectId: "oshop-d352e",
+    storageBucket: "",
+    messagingSenderId: "679359948016"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAPsG8Fw9kN0uvqBHFB5tDeeGvTVzTilDE",
+    authDomain: "oshop-d352e.firebaseapp.com",
+    databaseURL: "https://oshop-d352e.firebaseio.com",
+    projectId: "oshop-d352e",
+    storageBucket: "",
+    messagingSenderId: "679359948016"
+  }
 };
 
 /*
@@ -11,5 +19,25 @@ export const environment = {
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
  * import the following file, but please comment it out in production mode
  * because it will have performance impact when throw error
+ * 
+ * 
+<script src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAPsG8Fw9kN0uvqBHFB5tDeeGvTVzTilDE",
+    authDomain: "oshop-d352e.firebaseapp.com",
+    databaseURL: "https://oshop-d352e.firebaseio.com",
+    projectId: "oshop-d352e",
+    storageBucket: "",
+    messagingSenderId: "679359948016"
+  };
+  firebase.initializeApp(config);
+</script>
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
